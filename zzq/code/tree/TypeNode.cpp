9df120a60f0ebb47cb7d56d304b2219b779f4883
ZhangZhiQiang <2510406196@qq.com>
2020-11-29 17:53:29 +0800

    impl VardefNode

--- zzq/code/tree/TypeNode.cpp
+++ zzq/code/tree/TypeNode.cpp
@@ -2,22 +2,23 @@
 #include "IDNode.h"
 
 StructTypeNode * StructTypeNode::createNode(IDNode *_ID, ASTNode *_Members) {
-        StructTypeNode *result = getStructType(_ID->ID);
-        if (result) {
-            if (!result->defined)
-                result->addChild(_Members);
-            else if (_Members) {
-                // 报错
-                printf("重复定义struct %s", result->ID->ID);
-            }
-        } 
-        else {
-            result = new StructTypeNode;
-            result->ID = _ID;
-            result->name = _ID->ID;
-            result->defined = false;
-            result->addMembers(_Members);
-            structList.push_back(result);
+    StructTypeNode *result = getStructType(_ID->ID);
+    if (result) {
+        if (!result->defined)
+            result->addChild(_Members);
+        else if (_Members) {
+            // 报错
+            printf("重复定义struct %s", result->ID->ID);
         }
-        return result;
+    } 
+    else {
+        result = new StructTypeNode;
+        result->ID = _ID;
+        result->name = _ID->ID;
+        _ID->id_type = IDType_e::Typename;
+        result->defined = false;
+        result->addMembers(_Members);
+        structList.push_back(result);
+    }
+    return result;
 }
