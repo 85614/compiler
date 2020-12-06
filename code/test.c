8184fa5fad1259f1c613da8ccab6f3be2207ff60
NULL-1-bit <359643416@qq.com>
2020-12-06 21:37:15 +0800

    N

--- code/test.c
+++ code/test.c
@@ -1,18 +1,16 @@
-void swap(int** a1, int* b) {
-    int t = *a1;
-    *a1 = *b;
-    *b = t;
+int f(int n) {
+    if (n == 1) {
+        return 1;
+    } else if (n == 2) {
+        return 2;
+    }
+    // int a = f(n - 1);
+    // int b = f(n - 2);
+    int a = 0, b = -2 * 3;
+    return a + b;
 }
 
-int main()
-{
-    int a = 2;
-    int b = 3;
-    print_int(a);
-    print_int(b);
-    int* pa = &a;
-    int* pb = &b;
-    swap(pa, pb);
-    print_int(a);
-    print_int(b);
+int main() {
+    int d = f(5);
+    print_int(d);
 }
